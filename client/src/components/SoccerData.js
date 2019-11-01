c85aee1f1e3ef94d17d91490adc446e868cb5c67
rndpatel@gmail.com <rndpatel@gmail.com>
2019-11-01 09:21:29 -0700

    setup SoccerData with semantic UI cards

---
+++ client/src/components/SoccerData.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Card } from 'semantic-ui-react';
+
+const SoccerData = (props) => {
+
+
+    return (
+        <Card.Group>
+            <Card>
+                <Card.Content>
+                    <Card.Header></Card.Header>
+                    <Card.Meta></Card.Meta>
+                    <Card.Description></Card.Description>
+                </Card.Content>
+            </Card>
+        </Card.Group>
+    )
+}
+
+export default SoccerData;
