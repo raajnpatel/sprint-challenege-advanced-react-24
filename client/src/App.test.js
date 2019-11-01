ed06abdaf72c203d16d3f99377ab14bc7544f11f
rndpatel@gmail.com <rndpatel@gmail.com>
2019-11-01 13:42:48 -0700

    added another better test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { render, fireEvent } from "@testing-library/react";
+import { render, fireEvent, } from "@testing-library/react";
+import * as rtl from "@testing-library/react";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+
+it('App renders', async() => {
+  const app = await rtl.render(<App />);
+  const dark = await app.getByText(/dark mode/i);
+  rtl.fireEvent.click(dark);
+  let body = document.getElementsByTagName('body');
+  expect(body[0].classList.contains('dark-mode')).toBe(true);
+  rtl.fireEvent.click(dark);
+  expect(body[0].classList.contains('dark-mode')).toBe(false);
+})
 
 it('darkmode', () => {
-  // let clicked = false;
-  const {getByText} = render(<App useDarkMode = {() => clicked = true} />);
+  const {getByText} = render(<App />);
   const darkmode = getByText(/^dark mode$/i);
   fireEvent.click(darkmode);
-  // expect(clicked).toBe(true);
 });
 
 test('header displaying', () => {
--- client/src/components/SoccerClassComponent.js
+++ client/src/components/SoccerClassComponent.js
@@ -11,7 +11,7 @@ class SoccerClassComponent extends React.Component {
         };
     }
 
-    componentDidMount() {
+   async componentDidMount() {
         axios
             .get("http://localhost:5000/api/players")
             .then(response => {
